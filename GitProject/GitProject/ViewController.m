4151c563e847be56ddb627cb1ce13f51564eb58c
AbhiRKeesara <ARK@abhinays-MacBook-Pro.local>
2016-03-14 14:46:40 -0400

    new commit by abhi @ 2:46 PM

--- GitProject/GitProject/ViewController.m
+++ GitProject/GitProject/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     
     UIView *remoteServerView = [[UIView alloc] init];
+    // my new changes (abhi)
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
